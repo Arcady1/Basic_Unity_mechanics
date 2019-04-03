eb04e594a91284be84d8065ef7b442391c28eab3
Arcady <insite1029@gmail.com>
2019-04-03 14:46:43 +0300

    Changed

--- Билет №12/Assets/Scripts/CameraController.cs
+++ Билет №12/Assets/Scripts/CameraController.cs
@@ -6,16 +6,24 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     Vector3 distance, moveVec;
+    private float smootjSpeed = 3f;
 
     private void Start()
     {
-        distance = transform.position - player.transform.position;
+        distance = transform.position - player.transform.position;        
     }
 
     private void Update()
     {
         moveVec = distance + player.transform.position;
         moveVec.x = 0;
-        transform.position = moveVec;
+        moveVec.y = player.transform.position.y;
+
+        Vector3 playerPosition = player.transform.position;
+        if (playerPosition.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(moveVec.x, moveVec.y, moveVec.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smootjSpeed);
+        }
     }
 }
