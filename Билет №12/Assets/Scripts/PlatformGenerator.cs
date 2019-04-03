f741ab3e89b1bdb3add7455c5a691ae079c8407e
Arcady <insite1029@gmail.com>
2019-04-03 15:48:08 +0300

    Changed

--- Билет №12/Assets/Scripts/PlatformGenerator.cs
+++ Билет №12/Assets/Scripts/PlatformGenerator.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    
+    public GameObject platformPrefab;
+    public int numberOfPlatform;
+    public float posX = 3.5f;
+    public float minY = 3f;
+    public float maxY = 5f;
+
+    private void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfPlatform; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-posX, posX);
+            Instantiate(platformPrefab,spawnPosition, Quaternion.identity);
+        }
+    }
 }
