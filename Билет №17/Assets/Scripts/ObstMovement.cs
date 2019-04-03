5664c79c5f2c4a476740717b0db1e68758ce39a0
Arcady <insite1029@gmail.com>
2019-04-03 22:53:19 +0300

    New

---
+++ Билет №17/Assets/Scripts/ObstMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstMovement : MonoBehaviour
+{
+    private float obstacleSpeed = 5f;
+
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        Vector3 direction = -transform.right * obstacleSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, obstacleSpeed * Time.deltaTime);
+    }
+}
---
+++ Билет №17/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject obstacle;
+    private float StartTime = 0f;
+    public float Interval = 1.5f;
+    Vector3 movement;
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad > StartTime + Interval)
+        {
+            StartTime += Interval;
+
+            Vector3 spawnPosition = new Vector3();
+
+            if(spawnPosition.y == 0)
+            {
+                spawnPosition.y = -2.5f;
+                spawnPosition.x = 5f;
+
+            }
+
+            if(Random.Range(0, 2) == 0)
+            {
+                spawnPosition.x = 5f;
+                spawnPosition.y = -3.6f;
+                //Instantiate(obstacle, spawnPosition, Quaternion.identity);
+            }
+            if(Random.Range(0, 2) == 1)
+            {
+                spawnPosition.x = 5f;
+                spawnPosition.y = -2.5f;
+                //Instantiate(obstacle, spawnPosition, Quaternion.identity);
+            }
+
+            Instantiate(obstacle, spawnPosition, Quaternion.identity);
+        }
+    }
+}
