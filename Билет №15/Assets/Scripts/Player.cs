4cab2edaed72d9b306c9f7fc0eee1d7ab08b9a80
Arcady <insite1029@gmail.com>
2019-04-03 19:54:52 +0300

    New_Examination_Paper

---
+++ Билет №15/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speedMove = 9f;
+    private float gravityForce;
+    Vector3 moveVec;
+    CharacterController ch_controller;
+
+    public void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+    }
+    public void Update()
+    {
+        CharacterMove();
+        Graviy();
+    }
+
+    public void CharacterMove()
+    {
+        moveVec = Vector3.zero;
+        moveVec.x = Input.GetAxis("Horizontal") * speedMove;
+        moveVec.z = Input.GetAxis("Vertical") * speedMove;
+        moveVec.y = gravityForce;
+
+        ch_controller.Move(moveVec * Time.deltaTime);
+    }
+
+    private void Graviy()
+    {
+        if(!ch_controller.isGrounded)
+        {
+            gravityForce -= 20f * Time.deltaTime;
+        }
+        else
+        {
+            gravityForce = -1f;
+        }
+    }
+}
---
+++ Билет №15/Assets/Scripts/TargetGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetGenerator : MonoBehaviour
+{
+    public GameObject target;
+    private float minX = -4.5f;
+    private float maxX = 4.5f;
+    private float minZ = -5f;
+    private float maxZ = 5f;
+    public float Interval = 2f;
+    private float StartTime = 0f;
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad > Interval + StartTime)
+        {
+            StartTime += Interval;
+
+            Vector3 spawnPosition = new Vector3();
+
+            spawnPosition.x = Random.Range(minX, maxX);
+            spawnPosition.z = Random.Range(minZ, maxZ);
+
+            Instantiate(target, spawnPosition, Quaternion.identity);
+        }
+
+        //Destroy(target, 2f);
+    }
+
+
+}
