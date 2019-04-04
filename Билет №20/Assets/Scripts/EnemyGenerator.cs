122e25a804684dbacb2c1deb6e1bd2e4db60f7ab
Arcady <insite1029@gmail.com>
2019-04-04 15:00:14 +0300

    New

---
+++ Билет №20/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject enemy;
+    private float StartTime = 0;
+    public float Interval = 2f;
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad > StartTime + Interval)
+        {
+            StartTime += Interval;
+
+            Vector3 spawnPosition = new Vector3();
+
+            spawnPosition.x = Random.Range(-15f, 15f);
+            spawnPosition.y = 0.5f;
+            spawnPosition.z = Random.Range(-15f, 15f);
+
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }        
+    }
+}
---
+++ Билет №20/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 8f;
+    Vector3 moveVec;
+
+    CharacterController ch_controller;
+
+    private void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveVec.x = Input.GetAxis("Vertical") * playerSpeed;
+        moveVec.z = Input.GetAxis("Horizontal") * playerSpeed;
+
+        ch_controller.Move(moveVec * Time.deltaTime);
+    }
+}
