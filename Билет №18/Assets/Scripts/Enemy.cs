ae591ca9ecdb116c727c3eca281f405e3319ce98
Arcady <insite1029@gmail.com>
2019-04-04 10:40:47 +0300

    New

---
+++ Билет №18/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float enemySpeed = 7f;
+
+    void Update()
+    {
+        Vector3 direction = transform.right * enemySpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+    }
+}
---
+++ Билет №18/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    private float StartTime = 0f;
+    public float Interval = 1f;
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad > StartTime + Interval)
+        {
+            StartTime += Interval;
+
+            Vector3 spawnPosition = new Vector3();
+            spawnPosition.x = Random.Range(-3.5f, 3.5f);
+            spawnPosition.y = Random.Range(-4.5f, 4.5f);
+
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+    }
+}
